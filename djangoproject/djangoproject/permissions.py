0532976b5e174daf2c8437b551150b5b75a852c4
Vyshnavi Ravula <ravulavaishnavi2001@gmail.com>
2025-06-05 20:23:56 +0530

    made some improvements for patient app

--- djangoproject/djangoproject/permissions.py
+++ djangoproject/djangoproject/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission,SAFE_METHODS
 from clinic.models import Clinic
+from rest_framework.exceptions import PermissionDenied
 
 class IsSuperAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
@@ -22,8 +23,15 @@ class IsClinicAdminOfClinic(BasePermission):
                 return obj.clinic in request.user.clinics.all()
         return(False)
 
-class IsStaff(BasePermission):
+class IsClinicAdminOrStaffOfClinic(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.user.is_authenticated and request.user.role in ['ClinicAdmin',"SuperAdmin",'Staff']:
             return True
-        return request.user.is_authenticated and request.user.role=="Staff"
+        raise PermissionDenied("User is not authenticated or User is not having required permission.")  
+    def has_object_permission(self, request, view, obj):
+        if request.user.role=='SuperAdmin':
+            return True 
+        if request.user.role in ['ClinicAdmin','Staff']:
+            if hasattr(obj,'clinic'):
+                return obj.clinic in request.user.clinics.all()
+        return False
--- djangoproject/patient/views.py
+++ djangoproject/patient/views.py
@@ -2,16 +2,30 @@ from django.shortcuts import render
 from .serializers import PatientSerializer
 from .models import Patient
 from rest_framework import viewsets,exceptions
-from djangoproject import permissions
+from djangoproject.permissions import IsClinicAdminOrStaffOfClinic
 
 # Create your views here.
 class PatientManagement(viewsets.ModelViewSet):
     queryset=Patient.objects.all()
     serializer_class=PatientSerializer
-    permission_classes=[permissions.IsClinicAdminOfClinic]
+    permission_classes=[IsClinicAdminOrStaffOfClinic]
+
+    def get_queryset(self):
+        user=self.request.user
+        if user.role=='SuperAdmin':
+            return Patient.objects.all()
+        elif user.role in ['ClinicAdmin','Staff']:
+            if self.action=='list':
+                ids=user.clinics.values_list('id',flat=True)
+                return Patient.objects.filter(clinic__in=ids)
+            else:
+                return Patient.objects.all()
+        else:
+            raise exceptions.PermissionDenied("No matching patients found")
+
 
     def perform_create(self, serializer):
         clinic=serializer.validated_data['clinic']
-        if self.request.user.role in['ClinicAdmin','Superadmin'] and clinic not in self.request.user.clinics.all():
+        if self.request.user.role in['ClinicAdmin','Superadmin','Staff'] and clinic not in self.request.user.clinics.all():
             raise exceptions.PermissionDenied("Cannot add patient for specified clinic due to persmission issue")
         serializer.save()
