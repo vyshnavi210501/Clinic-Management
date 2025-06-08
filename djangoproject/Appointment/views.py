94dc4a40e40cad3d99751cf38474e67b446ab8fb
Vyshnavi Ravula <ravulavaishnavi2001@gmail.com>
2025-06-08 21:18:46 +0530

    overlapping appointment fix

--- djangoproject/Appointment/views.py
+++ djangoproject/Appointment/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Appointment
 from .serializer import AppointmentSerializer
 from djangoproject import permissions
+from datetime import datetime,date as current_Date
 from rest_framework.exceptions import PermissionDenied,ValidationError
 # Create your views here.
 class AppointmentViewset(ModelViewSet):
@@ -21,20 +22,30 @@ class AppointmentViewset(ModelViewSet):
             raise ValidationError('Therapist doesnt belong to the selected clinic')
         if patient.clinic!=clinic:
             raise ValidationError('Patient doesnt belong to selected clinic')
-        if self.request.user.role=='SuperAdmin':
-            serializer.save()
-            return
-        overlapping_appointments = Appointment.objects.filter(
+        if end_time<=start_time:
+            raise ValidationError('Start time must be before end time')
+
+        now=datetime.now()
+        if date<current_Date.today() or (date==current_Date and start_time<=now.time()):
+            raise ValidationError("Appointment must be scheduled for future time.")
+        if Appointment.objects.filter(
             therapist=therapist,
             date=date,
             status=Appointment.Status.BOOKED,
             start_time__lt=end_time,
             end_time__gt=start_time
-        )
-
-        if overlapping_appointments.exists():
+        ).exists():
             raise ValidationError('Therapist is already booked during this time slot.')
-        if clinic not in self.request.user.clinics.all():
+        
+        if Appointment.objects.filter(
+            patient=patient,
+            date=date,
+            status=Appointment.Status.BOOKED,
+            start_time__lt=end_time,
+            end_time__gt=start_time
+        ).exists():
+            raise ValidationError("Patient is already during this time slot.")
+        if self.request.user.role=='SuperAdmin' and clinic not in self.request.user.clinics.all():
             raise PermissionDenied('Cannot add appointment who is not unauthorized for this clinic')
         
         serializer.save()
