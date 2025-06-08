11785b0c80a05a87e8a95f627e667b8e40824667
Vyshnavi Ravula <ravulavaishnavi2001@gmail.com>
2025-06-08 21:44:16 +0530

    made some improvements for trackrecord app

--- djangoproject/track_record/views.py
+++ djangoproject/track_record/views.py
@@ -4,6 +4,7 @@ from .models import Track_Record
 from .serializer import TrackRecordSerializer
 from djangoproject import permissions
 from rest_framework import exceptions
+from datetime import date as current_Date
 
 # Create your views here.
 class TrackRecordViewset(ModelViewSet):
@@ -13,6 +14,16 @@ class TrackRecordViewset(ModelViewSet):
 
     def perform_create(self, serializer):
         clinic=serializer.validated_data["clinic"]
-        if clinic not in self.request.user.clinics.all():
+        therapist=serializer.validated_data["therapist"]
+        patient=serializer.validated_data["patient"]
+        date=serializer.validated_data["date"]
+        if date>current_Date.today():
+            raise exceptions.ValidationError("Trackrecord can be recorded after visiting.")
+        if self.request.user.role!='SuperAdmin' and clinic not in self.request.user.clinics.all():
             raise exceptions.PermissionDenied("Cannot create as its not assigned to clinic")
+        if clinic.id!=therapist.clinic_id:
+            raise exceptions.ValidationError("Therapist doesnt belong to clinic")
+        if clinic.id!=patient.clinic_id:
+            raise exceptions.ValidationError("patient doesnt belong to clinic")
+
         serializer.save()
